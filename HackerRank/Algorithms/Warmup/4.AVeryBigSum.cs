29c29b038a3bc71cae06d156ee7e6a2d4406fdec
mdrews <matthewdrews@gmail.com>
2017-05-22 21:52:13 -0400

    Adding Algorithms->Warmup->4.AVeryBigSum

---
+++ HackerRank/Algorithms/Warmup/4.AVeryBigSum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //string[] arr_temp = Console.ReadLine().Split(' ');
+        //int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+        int[] arr = new int[] { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 };
+        double sum = 0;
+        foreach(var number in arr)
+        {
+            sum += number;
+        }
+        Console.WriteLine("Sum is: {0}", sum);
+    }
+}
