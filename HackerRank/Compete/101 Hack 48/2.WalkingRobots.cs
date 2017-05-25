dddfed0e92ef8fd1987b7b7d4b6964278fd6e805
Matthew Drews <mdrews@unitedshore.com>
2017-05-25 14:46:52 -0400

    Adding 101 Hack 48 - 2 and 3

---
+++ HackerRank/Compete/101 Hack 48/2.WalkingRobots.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static int howManyCollisions(string s)
+    {
+        // Complete this function
+        int crashStart = -1;
+        int crashEnd = -1;
+        int numberStopped = 0;
+        for (int x = 0; x < s.Length; x++)
+        {
+            if (s[x] == 'r' || s[x] == 'd')
+            {
+                crashStart = x;
+                break;
+            }
+        }
+        if (crashStart == -1) { return 0; }
+        for (int x = (s.Length-1); x > 0; x--)
+        {
+            if (s[x] == 'l' || s[x] =='d')
+            {
+                crashEnd = x+1;
+                break;
+            }
+        }
+        if (crashEnd == -1) { return 0; }
+        for(int x = crashStart; x < crashEnd; x++)
+        {
+            if (s[x] == 'd'){ numberStopped++; }
+        }
+        return (crashEnd - crashStart - numberStopped);
+    }
+
+    static void Main(String[] args)
+    {
+        int q = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < q; a0++)
+        {
+            string s = Console.ReadLine();
+            // Returns the number of times moving robots collide.
+            int result = howManyCollisions(s);
+            Console.WriteLine(result);
+        }
+        //string s = "dddrddrrrlddrrrrrdlddldllrlldlllrlrrrddllrrrldldrlldlldllddlldldddldldrrddlrrrrdrdddllrrddddrdddlrdlldlldrldldlrrrldlrrrllrldlrrrdlrrldrldrlrlddrlllrddrdlrlrrdrllddllddrdrdddddlrrdrrldrdldlrrrdlrlddrddlrrlrrrdddldddrdllddlrlrlllrldrlllllrrrlrrdrrlllldldldddrllrldlllddlldlrldldrrrrdrrdrdlrllrdddddlddrddllrrdrrrlddrdldddddrrlldlrdddlrrdllrddrddllrrdrlrlrddddrrrlrlddldrrdrrddlrddrdddllrdddrlrlddlrdrdllrdrdrdldrddlldlddddddldrrddrrrdlrrdldrlldddldrldldlldrrrrrddlrldlddlrrldddlrlrldrldlddlddddlrddlrldrlrllllldrlrldddrdddlrlrllddlldlrdlrdrdldlrrrlrrrdrrddlldrdlrrdldlldrrrrldddlllrldddldrdrdlllrrdlrrdrrdddrllrrldlllllldrddlrrrrrrdrdrddldlddrdlllddddldldrdlldrdddldlldrdllldrdlrdrdllldrrlldrldrrdldrlrlrlddrddrrlrdrldrlrdrldldlldlddlddddrrlrldrdlldddlrrrrrlddrdrldlrdrdrdrdrddldrrdldrldldrldlrldlllddlrdllllrddrrdddldldrrlrlldrldrlrdddddrlllrddllrrrlddldlddlrrdlrrldddllrrlrdddrrrldrrldrlldlldlrdldrdrllrddrddlrddddld";
+        //Console.WriteLine(howManyCollisions(s));
+    }
+}
---
+++ HackerRank/Compete/101 Hack 48/3.CartesianCountry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _2.CartesianCountry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens_x1 = { "1", "1" };//Console.ReadLine().Split(' ');
+            long x1 = Convert.ToInt64(tokens_x1[0]);
+            long y1 = Convert.ToInt64(tokens_x1[1]);
+            string[] tokens_x2 = { "5", "4" };//Console.ReadLine().Split(' ');
+            long x2 = Convert.ToInt64(tokens_x2[0]);
+            long y2 = Convert.ToInt64(tokens_x2[1]);
+            string[] tokens_xC = { "2", "3" };//Console.ReadLine().Split(' ');
+            long xC = Convert.ToInt64(tokens_xC[0]);
+            long yC = Convert.ToInt64(tokens_xC[1]);
+            long total = 0;
+            long xResult = (xC - x1 <= x2 - xC) ? (xC - x1) : (x2 - xC);
+            long yResult = (yC - y1 <= y2 - yC) ? (yC - y1) : (y2 - yC);
+            //if (xResult % 2 == 0) xResult -= 1;
+            //if (yResult % 2 == 0) yResult -= 1;
+            long xTotal = ((xResult * 2) + 1);
+            long yTotal = ((yResult * 2) + 1);
+            total = ((xTotal * yTotal) / 2);
+            Console.WriteLine(total);
+        }
+    }
+}
