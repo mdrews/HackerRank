0199245af5b3b442afa6ab179fdf1934d2b61905
mdrews <matthewdrews@gmail.com>
2017-05-25 22:20:51 -0400

    Adding Algorithms->warmup->7.Staircase

---
+++ HackerRank/Algorithms/Warmup/7.Staircase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 6;
+
+            for(int x = 1; x <= n; x++)
+            {
+                for(int a = 0; a < n-x; a++)
+                {
+                    Console.Write(" ");
+                }
+                for (int b = 0; b < x; b++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
