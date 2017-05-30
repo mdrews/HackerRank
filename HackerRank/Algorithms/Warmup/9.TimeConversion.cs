e52a97d598ae25ccfb628b6947bf5971a6ac44f3
Matthew Drews <mdrews@unitedshore.com>
2017-05-30 09:40:47 -0400

    Adding Algorithms->Warmup->9.TimeConversion

---
+++ HackerRank/Algorithms/Warmup/9.TimeConversion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace TimeConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "07:05:45PM";
+            DateTime time = DateTime.ParseExact(input, "hh:mm:sstt", CultureInfo.InvariantCulture);
+            string result = time.ToString("HH:mm:ss");
+            Console.WriteLine(result);
+        }
+    }
+}
