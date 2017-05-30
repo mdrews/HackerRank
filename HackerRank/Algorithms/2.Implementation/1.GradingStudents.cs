5c84886a609f6779fc3adc3ed35ab2842b9f2cd0
Matthew Drews <mdrews@unitedshore.com>
2017-05-30 10:35:13 -0400

    Created Algorithms->Implementation, adding 1.Grading Students. Also moved Warmup to 1.Warmup to stay in line with the website

---
+++ HackerRank/Algorithms/2.Implementation/1.GradingStudents.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+static class Solution
+{
+
+    static int[] solve(int[] grades)
+    {
+        for (int i = 0; i < grades.Length; i++)
+        {
+            if (grades[i] > 35)
+            {
+                int mod = grades[i] % 5;
+                if (mod > 2)
+                {
+                    grades[i] += 5- mod;
+                }
+            }
+        }
+        return grades;
+    }
+
+    static void Main(String[] args)
+    {
+        //int n = Convert.ToInt32(Console.ReadLine());
+        //int[] grades = new int[n];
+        //for (int grades_i = 0; grades_i < n; grades_i++)
+        //{
+        //    grades[grades_i] = Convert.ToInt32(Console.ReadLine());
+        //}
+        int[] grades = { 73, 67, 38, 33 };
+
+        int[] result = solve(grades);
+        Console.WriteLine(String.Join("\n", result));
+
+
+    }
+}
