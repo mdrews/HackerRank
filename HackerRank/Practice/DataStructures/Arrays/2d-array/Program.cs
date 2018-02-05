f36c7cd0f2fe15942bca68657efcbe409ceda8d8
Matthew Drews <mdrews@uwm.com>
2018-02-05 09:44:16 -0500

    Add WeekOfCode36 - Day 1 (and other files)

---
+++ HackerRank/Practice/DataStructures/Arrays/2d-array/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _2d_array
+{
+    class Program
+    {
+        public static void HighestHourglass(int[][] array)
+        {
+            int highestHourglassSum = 0;
+            int currentHourglassSum = 0;
+            for (int x = 0; x < 4; x++)
+            {
+                for(int y = 0; y < 4; y++)
+                {
+                    currentHourglassSum = array[x][y] + array[x][y + 1] + array[x][y + 2]
+                                          + array[x + 1][y + 1] + array[x + 2][y] + array[x + 2][y + 1] + array[x + 2][y + 2];
+                    if (currentHourglassSum > highestHourglassSum)
+                    {
+                        highestHourglassSum = currentHourglassSum;
+                    }
+                    Console.WriteLine(currentHourglassSum);
+                }
+            }
+            Console.WriteLine(highestHourglassSum);
+        }
+
+        static void Main(string[] args)
+        {
+            int[][] arr = new int[6][]
+            {
+                new int[] {1, 1, 1, 0, 0, 0},
+                new int[] {0, 1, 0, 0, 0, 0},
+                new int[] {1, 1, 1, 0, 0, 0},
+                new int[] {0, 0, 2, 4, 4, 0},
+                new int[] {0, 0, 0, 2, 0, 0},
+                new int[] {0, 0, 1, 2, 4, 0}
+            };
+            
+            HighestHourglass(arr);
+            
+        }
+    }
+}
---
+++ HackerRank/WeekOfCode36/1.Acid-Naming/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1.Acid_Naming
+{
+    class Program
+    {
+        static string AcidNaming(string acid_name)
+        {
+            acid_name = acid_name.ToLower();
+            string acidType = "not an acid";
+            if (acid_name.EndsWith("ic"))
+            {
+                if (acid_name.StartsWith("hydro"))
+                {
+                    return "non-metal acid";
+                }
+                return "polyatomic acid";
+            }
+            return acidType;
+        }
+        static void Main(string[] args)
+        {
+            List<string> acids = new List<string>()
+            {
+                "3",
+                "hydrochloric",
+                "rainbowic",
+                "idontevenknow"
+            };
+            foreach (var acid in acids)
+            {
+                Console.WriteLine(AcidNaming(acid));
+            }
+            
+        }
+    }
+}
